0a0d42674b5a2002699372d9d597e3f9a00dd507
Rachel <hello.rachelwong@gmail.com>
2019-03-05 17:25:07 +1000

    Added continue/quit options at welcome
    Added victory, defeat and goodbye screen messages

--- main.rb
+++ main.rb
@@ -1,18 +1,42 @@
 require_relative "battle.rb"
 
 # Welcome message, always executes.
+puts " ---------------------------- "
 puts "Welcome to Monster Smash!"
+puts " ---------------------------- "
+puts "Do you want to (C)ontinue or (Q)uit?"
+puts " ---------------------------- "
 
-# Game loop, mostly (entirely?) methods of battle object
-bout = Battle.new([Frank, Jerry])
-until bout.outcome != :ongoing
-    bout.run_round
+command = gets.chomp.downcase
+
+case command
+    when "c"
+        # Game loop, mostly (entirely?) methods of battle object
+        bout = Battle.new([Frank, Jerry])
+        until bout.outcome != :ongoing
+            bout.run_round
+        end
+
+    # Defeat screen, conditionally executes
+    if bout.outcome == :combatant1win
+        puts " ---------------------------- "
+        puts "   You have been defeated."
+        puts " ---------------------------- "
+        # Victory screen, conditionally executes
+    elsif bout.outcome == :combatant0win
+        puts " ---------------------------- "
+        puts "   Congrats! You have won!"
+        puts " ---------------------------- "
+    # Quit screen, always executes
+    else
+        puts " ---------------------------- "
+        puts "      Thanks for playing!"
+        puts " ---------------------------- "
+    end
+    
+    when "q"
+        puts " ---------------------------- "
+        puts "          Goodbye!"
+        puts " ---------------------------- "
+            
 end
-
-# Victory screen, conditionally executes
-
-# Defeat screen, conditionally executes
-
-# Quit screen, always executes
-puts "Thanks for playing!"
-#beep
