74519b6bd6af68acd9e95e78cfcab4242bfd5eae
Matthew Puku <matthew.puku@gmail.com>
2019-03-06 15:18:56 +1000

    removed underscores in move name strings

--- src/move.rb
+++ src/move.rb
@@ -9,9 +9,9 @@ end
 
 # initialise new moves with respective damage counts and speed counts
 Smash = Move.new("Smash", 30, 50)
-Rapid_Attack = Move.new("Rapid_Attack", 15, 90)
+Rapid_Attack = Move.new("Rapid Attack", 15, 90)
 Thrash = Move.new("Thrash", 20, 70)
 Bash = Move.new("Bash", 35, 10)
-Boost_Speed = Move.new("Boost_Speed", 10, 90)
+Boost_Speed = Move.new("Boost Speed", 10, 90)
 Berserk = Move.new("Berserk", 50, 10)
-Leeching_Bite = Move.new("Leeching_Bite", 10, 10)
+Leeching_Bite = Move.new("Leeching Bite", 10, 10)
