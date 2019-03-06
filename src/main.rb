f4da963942ec2b54cea2b7465bdd5c22b0165e31
Rachel <hello.rachelwong@gmail.com>
2019-03-07 09:42:45 +1000

    commenting & purpose

--- src/main.rb
+++ src/main.rb
@@ -1,16 +1,20 @@
 require_relative "battle.rb"
 require_relative "messages.rb"
 
-welcome_screen # defined in messages.rb
+# Main.rb triggers the start of a battle outputing screens from messages.rb and initialising a new battle object from battle.rb
 
-# Game loop, mostly (entirely?) methods of battle object
+welcome_screen # call welcome screen from messages.rb
+
+# Commence gaming loop handled in battle.rb with two combatants, user and computer
 bout = Battle.new([Player, Dracula])
 bout.display_commencement
+
+# Continue to run the battle until HP is exhausted
 until bout.outcome != :ongoing
     bout.run_round
 end
 
-# Ending screens
+# Display screens depending on outcomes (defeat, victory, draw)
 if bout.outcome == :combatant1win    # Computer victory (player defeat)
     defeat_screen                       # defined in messages.rb
 elsif bout.outcome == :combatant0win # Player victory
@@ -19,4 +23,5 @@ elsif bout.outcome == :draw          # Draw. Not possible with current moveset T
     puts "It's a draw!"                 # placeholder. 
 end
 
+# Where HP is exhausted or Quit is triggered, game over screen
 quit_screen # defined in messages.rb
