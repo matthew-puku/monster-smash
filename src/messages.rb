1780d80d032d349de40b706595342e23de265d15
Matthew Puku <matthew.puku@gmail.com>
2019-03-06 20:52:58 +1000

    completed majority of message methods with the help of io/console and artii gems

--- src/messages.rb
+++ src/messages.rb
@@ -1,3 +1,8 @@
+require 'bundler/setup'
+require 'rubygems'
+require 'artii'
+require 'io/console'
+
 def slow_puts(message, delay) # this might be gemmable
     # for character in message string:
         # clear screen
@@ -8,17 +13,45 @@ def slow_puts(message, delay) # this might be gemmable
 end
 
 def welcome_screen
-    # puts a bunch of stuff. Experiment here with colorize, ascii and slow_puts
+    a = Artii::Base.new
+    puts
+    puts "------------------------------   WELCOME TO   -------------------------------"
+    puts
+    puts a.asciify("Monster Smash!")
+    puts "------------------------ PRESS ANY KEY TO CONTINUE --------------------------"
+    STDIN.getch                                                                                                              
+    print "            \r" # extra space to overwrite in case next sentence is short
 end
 
 def victory_screen
-    # puts a bunch of stuff. Experiment here with colorize, ascii and slow_puts
+    b = Artii::Base.new
+    puts
+    puts "------------------------------   VICTORY   -------------------------------"
+    puts
+    puts b.asciify("           YOU WON !")
+    puts "------------------------ PRESS ANY KEY TO CONTINUE --------------------------"
+    STDIN.getch                                                                                                              
+    print "            \r" # extra space to overwrite in case next sentence is short
 end
 
 def defeat_screen
-    # puts a bunch of stuff. Experiment here with colorize, ascii and slow_puts
+    c = Artii::Base.new
+    puts
+    puts "-------------------------------   DEFEATED   --------------------------------"
+    puts
+    puts c.asciify("         YOU LOST !")
+    puts "------------------------ PRESS ANY KEY TO CONTINUE --------------------------"
+    STDIN.getch                                                                                                              
+    print "            \r" # extra space to overwrite in case next sentence is short
 end
 
 def quit_screen
-    # puts a bunch of stuff. Experiment here with colorize, ascii and slow_puts
+    d = Artii::Base.new
+    puts
+    puts "---------------------------   HASTALA VISTA   ---------------------------"
+    puts
+    puts d.asciify("  GAME OVER !")
+    puts "------------------------ PRESS ANY KEY TO QUIT --------------------------"
+    STDIN.getch                                                                                                              
+    print "            \r" # extra space to overwrite in case next sentence is short
 end
