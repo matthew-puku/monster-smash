85be67fc132eb1777b96684c2d70929691021916
Matthew Puku <matthew.puku@gmail.com>
2019-03-06 14:50:23 +1000

    added skeletal messages.rb file and changed main.rb to reference it.

--- src/main.rb
+++ src/main.rb
@@ -1,42 +1,26 @@
 require_relative "battle.rb"
+require_relative "messages.rb"
 
-# Welcome message, always executes.
-puts " ---------------------------- "
-puts "Welcome to Monster Smash!"
-puts " ---------------------------- "
-puts "Do you want to (C)ontinue or (Q)uit?"
-puts " ---------------------------- "
+welcome_screen # defined in messages.rb
 
 command = gets.chomp.downcase
 
 case command
-    when "c"
-        # Game loop, mostly (entirely?) methods of battle object
-        bout = Battle.new([Frank, Jerry])
-        until bout.outcome != :ongoing
-            bout.run_round
-        end
-
-    # Defeat screen, conditionally executes
-    if bout.outcome == :combatant1win
-        puts " ---------------------------- "
-        puts "   You have been defeated."
-        puts " ---------------------------- "
-        # Victory screen, conditionally executes
-    elsif bout.outcome == :combatant0win
-        puts " ---------------------------- "
-        puts "   Congrats! You have won!"
-        puts " ---------------------------- "
-    # Quit screen, always executes
-    else
-        puts " ---------------------------- "
-        puts "      Thanks for playing!"
-        puts " ---------------------------- "
+when "c"
+    # Game loop, mostly (entirely?) methods of battle object
+    bout = Battle.new([Frank, Jerry])
+    until bout.outcome != :ongoing
+        bout.run_round
     end
-    
-    when "q"
-        puts " ---------------------------- "
-        puts "          Goodbye!"
-        puts " ---------------------------- "
-            
+
+    if bout.outcome == :combatant1win # computer victory (player defeat)
+        defeat_screen # defined in messages.rb
+    elsif bout.outcome == :combatant0win # player victory
+        victory_screen # defined in messages.rb
+    elsif bout.outcome == :draw
+        puts "It's a draw!" # placeholder. This will probably just trigger a loss in final version.
+    end
+
 end
+
+quit_screen # defined in messages.rb
---
+++ src/messages.rb
@@ -0,0 +1,24 @@
+def slow_puts(message, delay) # this might be gemmable
+    # for character in message string:
+        # clear screen
+        # puts that character and all previous characters
+        # wait for (delay * some sensible constant)
+    # end
+    # wait even longer
+end
+
+def welcome_screen
+    # puts a bunch of stuff. Experiment here with colorize, ascii and slow_puts
+end
+
+def victory_screen
+    # puts a bunch of stuff. Experiment here with colorize, ascii and slow_puts
+end
+
+def defeat_screen
+    # puts a bunch of stuff. Experiment here with colorize, ascii and slow_puts
+end
+
+def quit_screen
+    # puts a bunch of stuff. Experiment here with colorize, ascii and slow_puts
+end
