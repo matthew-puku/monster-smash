574d7a7dfe38618b9605bd62f0ec2f6cd9ed27b5
Matthew Puku <matthew.puku@gmail.com>
2019-03-06 18:03:00 +1000

    added input validation and comments to run_round class method.

--- src/battle.rb
+++ src/battle.rb
@@ -30,26 +30,44 @@ class Battle
     #     puts "#{target.name} has #{target.current_HP.to_i}HP remaining."
     # end
     def run_round
-        puts display_choices(@combatants[0])
-        combatant0move = @combatants[0].search_moves(gets) # must add validation and ability to quit here. if quit set outcome to :quit and break
-        combatant1move = @combatants[1].random_move
-        if combatant0move.speed + @player_0_speed_advantage > combatant1move.speed
+        # Move selection. Dev note: this code could be replaced to create a 2 player game or a computer vs. computer auto-battle
+        validating_input = true
+        while validating_input # In this loop, the player can choose a move or quit
+            puts display_choices(@combatants[0])
+            user_input = gets.chomp.downcase
+            search_result = combatants[0].search_moves(user_input) # Storing in a variable to avoid running the function twice (once for conditional and once to store a success)
+            if search_result != nil # executes if valid combat input was entered
+                combatant0move = search_result # Dev note: Reassigning this variable is not strictly necessary, but makes code more readable and modular
+                validating_input = false # exits while loop
+            elsif user_input == "q" or user_input == "quit"
+                @outcome = :quit # change bout outcome to inform main.rb of user desire to quit.
+                return # break out of the function. Dev note: there may be a nicer way to do this but time does not permit research.
+            else
+                puts "Invalid input! Please try again."
+            end
+        end
+        combatant1move = @combatants[1].random_move # The computer chooses a move randomly
+
+        # Once moves are selected, decide who will go first
+        if combatant0move.speed + @player_0_speed_advantage > combatant1move.speed # Player first
             first_move = combatant0move
             first_mover = @combatants[0]
             second_move = combatant1move
             second_mover = @combatants[1]
-        else
+        else                                                                       # Computer first
             first_move = combatant1move
             first_mover = @combatants[1]
             second_move = combatant0move
             second_mover = @combatants[0]
         end
+
+        # Fight!
         first_move.use!(first_mover, second_mover)
-        update_outcome! # Check the move didn't end the battle before continuing
-        if outcome == :ongoing 
+        update_outcome! # Keep main.rb updated on battle state.
+        if outcome == :ongoing # Check the move didn't end the battle before continuing
             second_move.use!(second_mover, first_mover)
         end
-        update_outcome!
+        update_outcome! # Keep main.rb updated on battle state.
     end
 end
 
