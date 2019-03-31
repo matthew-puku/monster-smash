346d26377d78fcf0685605985732ce358d2f4f58
Matthew Puku <matthew.puku@gmail.com>
2019-03-31 12:49:11 +1000

    added power attribute that governs attack strength

--- src/monster.rb
+++ src/monster.rb
@@ -2,18 +2,21 @@ require_relative "move.rb"
 
 class Monster
     attr_reader :name, :moves, :default_dodge, :default_max_HP
-    attr_accessor :max_HP, :current_HP, :dodge
+    attr_accessor :max_HP, :current_HP, :dodge, :power
     
-    def initialize(name, max_HP, dodge, moves)
+    def initialize(name, max_HP, dodge, moves, power = 100)
         @name = name              # A string, e.g. "Dracula"
+        @moves = moves            # An array of Move objects.
         @current_HP = max_HP      # An integer, initialised to the same value as @max_HP. When this hits 0, the monster is defeated.
         @max_HP = max_HP          # An integer. If a monster's HP is restored, it cannot go above this value.
         @dodge = dodge            # An integer. Determines the likelihood of being hit by attacks. 50 is default.
                                       # Use 1000 when a monster should be unhittable by normal means
+        @power = power            # An integer. Multiplies the damage of Attack moves. Use 100 to deal default damage, 150 to deal 50% extra, etc.
         @default_dodge = dodge    # The default value of dodge. If a move removes buffs/debuffs, dodge will be set to this stat.
         @default_max_HP = max_HP  # As above, but for max_HP
-        @moves = moves            # An array of Move objects.
+        @default_power = power    # As above, but for power
     end
+
     def search_moves(input) # Returns a move from @moves based on the input string.
         input = input.downcase
         found_move = nil # Last resort, returned if no matches found.
@@ -28,9 +31,11 @@ class Monster
         end
         return found_move
     end
+
     def random_move # Returns a random move from @moves
         return @moves.sample
     end
+
     def display_moves # Formats @moves into a nice string that encourages correct user input and returns it. Intended to be cocenated with a string that allows the user to quit.
         output = "Choose a move: "
         for move in @moves do # Puts parenthesis around the (f)irst (l)etter of each move and cocenates it to the output.
